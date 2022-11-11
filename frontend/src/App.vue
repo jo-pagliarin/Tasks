a13981cde4676f12cfaad2fcee263c4e68ec5847
JoanaPagliarin <joanapagliarin5@gmail.com>
2022-11-11 14:29:03 -0300

    Não consegui adicionar font-awesome

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// createApp(App)
+// .component('font-awesome-icon', FontAwesomeIcon)
+// .mount('#app')
 
 const tarefas = ref([])
 const descricao = ref("")
@@ -33,6 +40,7 @@ async function deletar(tarefa) {
 }
 
 carregar_tarefas()
+
 </script>
 
 <template>
@@ -43,7 +51,10 @@ carregar_tarefas()
     <div  v-for="tarefa in tarefas" :key="tarefa.id">
       <div class="row">
         <div class="col-1">
-          <input type="checkbox" v-model="tarefa.concluido" @change="concluido(tarefa)" />
+          <label class="caixa">
+            <input class="checkbox" type="checkbox" v-model="tarefa.concluido" @change="concluido(tarefa)" />
+          <span class="checkmark"></span>
+          </label>
         </div>
         <div class="tarefa col-5">
           <s v-if="tarefa.concluido">{{ tarefa.descricao }}</s>
@@ -54,17 +65,26 @@ carregar_tarefas()
           {{ tarefa.dtCriacao }}
         </div>
         <div class="col-1">
-          <button type="submit" class="btn-delete" @click="deletar(tarefa)">X</button>
-        </div>
+          <button type="submit" class="btn-delete" @click="deletar(tarefa)">X
+              <!-- <div id="app">
+                <font-awesome-icon icon="fa-light fa-delete-left"/>
+              </div> -->
+          </button>
+        </div>          
       </div>
     </div>
   </div>
+  
 </template>
 
+<!-- <script>
+  export default {
+    name: 'App'
+  }
+</script> -->
+
+
 <style scoped>
-body {
-  color: rgb(255, 225, 225);
-}
 
 h1 {
   margin-top: 25px;
@@ -85,6 +105,8 @@ h1 {
 
 .container {
   text-align: center;
+  background-color: rgb(241, 228, 228);
+  border-radius: 25px;
 }
 
 .descricao {
@@ -99,14 +121,20 @@ h1 {
 .btn-submit {
   font-family: georgia;
   border-radius: 25px;
-  border-color: rgb(236, 42, 74);
+  border-color: black;
   width: 600px;
   letter-spacing: 3px;
   margin-top: 5px;
+  background-color: rgb(236, 42, 74);;
 }
 
-button:hover{
+button:hover.btn-submit{
   transform: scale(1.1);
+  transition: all 0.3s;
+}
+
+button:hover.btn-delete{
+  transform: scale(1.5);
   transition: all 0.3s;
 }
 
@@ -116,5 +144,71 @@ button:hover{
   font-family: georgia;
   font-weight: bolder;
 }
+
+.caixa {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 6px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  font-size: 16px;
+}
+/* Hide the browser's default checkbox */
+.caixa input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 21px;
+  width: 21px;
+  border-radius: 2px;
+  background-color: rgb(236, 42, 74);
+  border: 1px solid #ccc;
+}
+/* On mouse-over, add a grey background color */
+.caixa:hover input ~ .checkmark {
+  background-color: rgb(236, 42, 74);;
+  transform: scale(1.5);
+  transition: all 0.3s;
+}
+
+.caixa input:checked ~ .checkmark {
+  background-color: rgb(236, 42, 74);
+}
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+/* Show the checkmark when checked */
+.caixa input:checked ~ .checkmark:after {
+  display: block;
+}
+/* Style the checkmark/indicator */
+.caixa .checkmark:after {
+  left: 7px;
+  top: 0px;
+  width: 7px;
+  height: 15px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
 
 </style>
